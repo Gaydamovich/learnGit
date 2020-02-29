f82fca98b8dbbbab6d560674a4f3fecc5569fb35
Dmitry Gaydamovich <gaydamovich1992@mail.ru>
2020-02-29 13:10:26 +0300

    feature_3 add func

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-var Index;
-(function (Index) {
-    Index[Index["iphone"] = 1] = "iphone";
-    Index[Index["samsung"] = 3] = "samsung";
-})(Index || (Index = {}));
-var obj = {
-    s: {
-        f: {
-            a: 3,
-            g: {
-                g: 'hello'
-            }
-        }
+var sum = function () {
+    var rest = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        rest[_i] = arguments[_i];
     }
+    return rest.reduce(function (acc, elem) { return acc + elem; }, 0);
 };
-var copy = __assign(__assign({}, obj), { s: __assign(__assign({}, obj.s), { f: __assign(__assign({}, obj.s.f), { a: null }) }) });
-console.log(copy);
-console.log(copy === obj);
+console.log(sum(124, 3, 99, 88));
