c22370279d6db9f88b0f05c2318e9bc5e3a9bba3
Dmitry Gaydamovich <gaydamovich1992@mail.ru>
2020-02-29 14:32:48 +0300

    feature singl

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-var form = document.querySelector('form');
-document.querySelector('#pass').addEventListener('input', function () {
-    console.log('erqwerq');
-    if (this.value.length < 10) {
-        this.style.border = '1px solid red';
-    }
-    else {
-        this.style.border = 'none';
-    }
-});
-var validatePassword = function () {
-    var field = document.querySelector('#pass');
-    if (field.value.length < 10) {
-        alert('stop!!!!!');
-        return;
-    }
-    if (!field.value.match(/\d/)) {
-        alert('digit needed for password');
-    }
-};
-var eventSubmit = function (event) {
-    event.preventDefault();
-    validatePassword();
-};
-form.addEventListener('submit', eventSubmit);
+class Port {
+  constructor(field) {
+    if (Port.instence) return Port.instence
+
+    this.field = field
+
+    Port.instence = this
+  }
+}
+
+console.log(new Port('8080'));
+console.log(new Port('9000'));
