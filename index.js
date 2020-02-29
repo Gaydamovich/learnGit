e3a0a7b24d4fbbfcaae53305e3813b74d21da166
Dmitry Gaydamovich <gaydamovich1992@mail.ru>
2020-02-29 14:32:48 +0300

    feature singl

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+class Port {
+  constructor(field) {
+    if (Port.instence) return Port.instence
 
+    this.field = field
+
+    Port.instence = this
+  }
+}
+
+console.log(new Port('8080'));
+console.log(new Port('9000'));
