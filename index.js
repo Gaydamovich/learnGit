ba947490b8b1f2794c76349e1c1d7f3ce338bdac
Dmitry Gaydamovich <gaydamovich1992@mail.ru>
2020-02-29 21:23:16 +0300

    feature_3 edit

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var form = document.querySelector('form');
+document.querySelector('#pass').addEventListener('input', function () {
+    console.log('erqwerq');
+    if (this.value.length < 10) {
+        this.style.border = '1px solid red';
+    }
+    else {
+        this.style.border = 'none';
+    }
+});
 var validatePassword = function () {
     var field = document.querySelector('#pass');
     if (field.value.length < 10) {
         alert('stop!!!!!');
+        return;
+    }
+    if (!field.value.match(/\d/)) {
+        alert('digit needed for password');
     }
 };
 var eventSubmit = function (event) {
+    event.preventDefault();
     validatePassword();
 };
 form.addEventListener('submit', eventSubmit);
