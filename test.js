234afebeea765083926931134b3cc538ee5dedf8
Dmitry Gaydamovich <gaydamovich1992@mail.ru>
2020-03-13 23:05:50 +0300

    feature_3 add classes

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
-console.log(3)
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age
+  }
+}
+
+class Student extends Person {
+  constructor(name, age, nick) {
+    super(name, age);
+    this.nick = nick
+  }
+  get nickname () {
+    console.log(this.nick)
+  }
+}
+
+const st = new Student('dima', 34, 'lev');
+st.nickname;
