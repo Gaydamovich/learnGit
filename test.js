a755b3afa76768e7dc4401e8a3f631ea38949139
Dmitry Gaydamovich <gaydamovich1992@mail.ru>
2020-03-14 12:31:35 +0300

    master: resolve conflicts

--- test.js
+++ test.js
@@ -3,6 +3,10 @@ class Person {
     this.name = name;
     this.age = age
   }
+
+  say() {
+    console.log('hi')
+  }
 }
 
 class Student extends Person {
@@ -10,10 +14,53 @@ class Student extends Person {
     super(name, age);
     this.nick = nick
   }
-  get nickname () {
-    console.log(this.nick)
+  say() {
+    super.say();
+    console.log('by')
   }
 }
 
 const st = new Student('dima', 34, 'lev');
-st.nickname;
+st.say();
+
+document.getElementById('in').addEventListener('input', (e) => {
+  console.log(e.data)
+});
+
+document.querySelector('#color').addEventListener('input', e => {
+  console.log(e.target.value)
+});
+
+document.querySelector('#date').addEventListener('input', e => {
+  console.log(e.target.value)
+});
+
+document.getElementById('sel').addEventListener('change', function () {
+  console.log(this.value)
+});
+
+document.getElementById('check').addEventListener('input', function (e) {
+  console.log(e.target.checked)
+});
+
+document.querySelectorAll('.i').forEach(el => {
+  el.addEventListener('input', (e) => {
+    console.log(e.target.value)
+  })
+});
+
+/**
+ *  Pattern Singleton
+ */
+class Single {
+  constructor(port) {
+    if (Single.inst) return Single.inst;
+
+    this.port = port;
+
+    Single.inst = this;
+    return Single.inst;
+  }
+}
+// console.log(new Single('8080'));
+// console.log(new Single('3033'));
